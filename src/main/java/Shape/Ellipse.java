d0cac0ea6deef3ecd47efaf6928e88f60ff0dfce
noah-blanchard <matanaworgen@gmail.com>
2024-01-23 15:23:42 -0500

    all shapes done

--- src/main/java/Shape/Ellipse.java
+++ src/main/java/Shape/Ellipse.java
@@ -2,6 +2,7 @@ package Shape;
 
 import Point.Point2d;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Ellipse extends BaseShape {
@@ -11,7 +12,23 @@ public class Ellipse extends BaseShape {
      * @param heightDiameter Height of the Ellipse
      */
     public Ellipse(Double widthDiameter, Double heightDiameter) {
+        super();
+        Double a = widthDiameter / 2;
+        Double b = heightDiameter / 2;
+        ArrayList<Point2d> coords = new ArrayList<Point2d>();
 
+        // Loop through a grid of points
+        for (double y = -b; y <= b; y += 0.5) {
+            for (double x = -a; x <= a; x += 0.5) {
+                // Check if the point (x, y) is inside the ellipse
+                if ((Math.pow(x, 2) / Math.pow(a, 2)) + (Math.pow(y, 2) / Math.pow(b, 2)) <= 1) {
+                    coords.add(new Point2d(x, y));
+                }
+            }
+        }
+
+        // Add all the points to your ellipse
+        addAll(coords);
     }
 
     /** TODO
@@ -19,7 +36,7 @@ public class Ellipse extends BaseShape {
      * @param dimensions 2D point containing the width and height of the Ellipse
      */
     public Ellipse(Point2d dimensions) {
-
+        this(dimensions.X(), dimensions.Y());
     }
 
     /**
@@ -27,7 +44,8 @@ public class Ellipse extends BaseShape {
      * @param coords Collection of 2D points
      */
     private Ellipse(Collection<Point2d> coords) {
-
+        super();
+        addAll(coords);
     }
 
     /** TODO
@@ -35,6 +53,6 @@ public class Ellipse extends BaseShape {
      */
     @Override
     public Ellipse clone() {
-        return null;
+        return (Ellipse) super.clone();
     }
 }
